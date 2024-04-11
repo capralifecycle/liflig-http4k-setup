c786a2318d64fc514de8ccae02c40b7d0d6b0e52
Trond Engell <trond.engell@gmail.com>
2024-04-11 14:59:44 +0200

    fix: Improve Kdoc.

--- src/main/kotlin/no/liflig/http4k/setup/LifligBasicApiSetup.kt
+++ src/main/kotlin/no/liflig/http4k/setup/LifligBasicApiSetup.kt
@@ -20,18 +20,19 @@ import org.http4k.filter.ServerFilters.CatchLensFailure
 import org.http4k.lens.RequestContextKey
 
 /**
- * Encapsulates the basic AP setup for all of our services so that they handle requests similarly.
+ * Encapsulates the basic API setup for all of our services so that they handle requests similarly.
  * It contains default core filters to be used in Liflig projects that provide the following
  * functionality:
  * - Log in json-format containing metadata about request. E.g. log id, request chain id, user info,
  *   headers, exception stacktrace etc.
+ * - Sets up default filters in a specific order so that log is enriched properly with data.
  * - Catching unhandled exceptions and respond in standard json-format.
  * - OpenTelemetry setup for recording exceptions.
  * - Sets Cors policy for API.
- * - Standard way of handling validation errors by lens failure (E.g. invalid request param) and
- *   respond in standard json-format.
- * - Convenience function for explicit handling of errors that helps to return error response in
- *   standard error format and logs throwable in API request log.
+ * - Standard way of handling validation errors by lens failure in contract APIs (E.g. invalid
+ *   request param) and respond in standard json-format.
+ * - Convenience function for explicit handling of application errors that helps to return error
+ *   response in standard error format and logs throwable in API request log.
  *
  * Note! Ordering of filters are important. Do not mess with them unless you know what you are
  * doing.
